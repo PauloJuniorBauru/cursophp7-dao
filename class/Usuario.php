7cd2066569b73370ddc06b28f815bb174c068aee
Paulo Junior <juniorviol@gmail.com>
2018-09-06 13:44:30 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -17,7 +17,7 @@ class Usuario {
 	public function setDessenha($value) { $this->dessenha = $value; }
 	public function setDtcadastro($value) { $this->dtcadastro = $value; }
 
-	# CARREGUE PELO ID
+	# CONSULTA USUÁRIO PELO ID
 	public function loadById($id) {
 		$sql = new Sql();
 		$result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
@@ -30,6 +30,36 @@ class Usuario {
 			$this->setDessenha($row["dessenha"]);
 			$this->setDtcadastro(new DateTime($row["dtcadastro"]));
 		}
+	}
+
+	# CONSULTA TODOS OS USUÁRIOS
+	public static function getList() {
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	# CONSULTA QUALQUER USUÁRIO - DE ACORDO COM A REGRA "LIKE"
+	public static function search($login) {
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(":SEARCH"=>"%$login%"));
+	}
+
+	# CONSULTA USUÁRIO ATRAVÉS DE AUTENTICAÇÃO
+	public function login($login, $password) {
+		$sql = new Sql();
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", 
+			array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+
+		if (count($result) > 0) {
+			$row = $result[0];
+
+			$this->setIdusuario($row["idusuario"]);
+			$this->setDeslogin($row["deslogin"]);
+			$this->setDessenha($row["dessenha"]);
+			$this->setDtcadastro(new DateTime($row["dtcadastro"]));			
+		}
+		else 
+			throw new Exception("Login e/ou Senha INVÁLIDOS!");			
 	}
 
 	# PARA STRING
--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php  
 require_once("config.php");
 
+/*Consulta um usuário pelo ID
 $user = new Usuario();
 $user->loadById(5);
 
 echo $user;
+*/
+
+
+/*Consulta todos usuários registrados 
+$list = Usuario::getList();
+
+echo json_encode($list);
+*/
+
+
+/*Consulta todos usuário(s) pelo Login, seguindo a regra SQL LIKE '%value%'
+$buscar = Usuario::search("paulo");
+
+echo json_encode($buscar);
+*/
+
+
+//CONSULTA USUÁRIO ATRAVÉS DE AUTENTICAÇÃO
+$user = new Usuario();
+$user->login("Paulo Florencio Junior", 123);
+
+echo $user;
+
 
 ?>
